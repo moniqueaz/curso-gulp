7e59bb312e12e7588ab3f8b06fb2c1e491a206fc
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-11 14:42:37 -0300

    Funcoes de concatena arquivos js, minimizar arquivos js e css (uglify, usemin, cssmin, htmlreplace)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
+
+    gulp.task('default', ['copy'], function(){
+        gulp.start('build-img', 'usemin')
+    });
 
     gulp.task('copy', ['clean'], function(){
         return gulp.src('src/**/*')
@@ -12,9 +21,18 @@ var gulp = require('gulp'),
             .pipe(clean()); // assincrono
     });
 
-    gulp.task('build-img', ['copy'], function(){
+    gulp.task('build-img', function(){
         // origem
         gulp.src('dist/img/**/*')
             .pipe(imagemin())
             .pipe(gulp.dest('dist/img')) // destino
     });
+
+    gulp.task('usemin', function() {
+        return gulp.src('dist/**/*.html')
+          .pipe(usemin({
+            js: [uglify],
+            css: [cssmin]
+          }))
+          .pipe(gulp.dest('dist'));
+      });
