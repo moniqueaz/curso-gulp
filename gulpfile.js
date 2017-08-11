81a6cf34bf7791f11bba4e8107bb0109ea898f6e
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-11 11:29:48 -0300

    Mudança na versão do gulp, gulp-imagmin e gulp-clean / Funcao para apagar diretorio de copia(dist) e copiar diretorio original nao otimizado(src) de forma sincrona

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
-    gulp.task('build-img', function(){
+    gulp.task('copy', ['clean'], function(){
+        return gulp.src('src/**/*')
+            .pipe(gulp.dest('dist')); // assincrono
+    });
+
+    gulp.task('clean', function(){
+        return gulp.src('dist')
+            .pipe(clean()); // assincrono
+    });
+
+    gulp.task('build-img', ['copy'], function(){
         // origem
-        gulp.src('src/img/**/*')
+        gulp.src('dist/img/**/*')
             .pipe(imagemin())
-            .pipe(gulp.dest('src/img')) // destino
+            .pipe(gulp.dest('dist/img')) // destino
     });
